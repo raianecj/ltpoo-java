3592793d35b31a00c73edf5a597dd330e16c3d75
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-08-15 22:01:12 -0300

    Update Main.java

--- aula-01/atividade/principal/Main.java
+++ aula-01/atividade/principal/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args) {
         ClasseVendas c = new ClasseVendas();
         c.setValor(Integer.valueOf(JOptionPane.showInputDialog("Digite um valor")));  
-        JOptionPane.showMessageDialog(null, c.calcularDesconto());
+        JOptionPane.showMessageDialog(null, "Novo valor é R$"+c.calcularDesconto());
     }
 }
