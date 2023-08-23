7bed972c5dc68b3e9458dd2c3263bb1dc6ab86d4
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-08-22 21:46:15 -0300

    Update Main.java

--- aula-03/atividade-parcelas/Main.java
+++ aula-03/atividade-parcelas/Main.java
@@ -1,14 +1,14 @@
 package br.com.visao;
 
-import br.com.controle.Calculos;
+import br.com.controle.Vendas;
 import javax.swing.JOptionPane;
 
 
 public class Main {
     public static void main(String[] args){
-        Calculos c = new Calculos();
-        c.setValor(Integer.valueOf(JOptionPane.showInputDialog("Digite o valor da venda")));  
-        c.setParcelas(Integer.valueOf(JOptionPane.showInputDialog("Digite a quantidade de parcelas")));  
-        JOptionPane.showMessageDialog(null, "Novo valor é R$"+c.calcular());
+        Vendas v = new Vendas();
+        v.setValor(Integer.valueOf(JOptionPane.showInputDialog("Digite o valor da venda")));  
+        v.setParcelas(Integer.valueOf(JOptionPane.showInputDialog("Digite a quantidade de parcelas")));  
+        JOptionPane.showMessageDialog(null, "Novo valor é R$"+v.calcular());
     }
 }
