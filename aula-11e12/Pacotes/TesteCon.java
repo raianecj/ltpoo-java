8c5f4a38393334b2ab290c2c94285a325d4d215a
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-10-31 20:52:02 -0300

    Create TesteCon.java

---
+++ aula-11e12/Pacotes/TesteCon.java
@@ -0,0 +1,14 @@
+//Classe criada dentro do pacote "entidade"
+
+package entidade;
+
+import java.sql.SQLException;
+
+
+public class TesteCon {
+    public static void main (String[] args) throws SQLException {
+        DAO cx = new DAO();
+        cx.abrirBanco();
+    }
+}
+
