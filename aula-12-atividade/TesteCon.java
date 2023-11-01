4e6fd6f6d53ffcac7e2323e59dffa87a90e472a2
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-10-31 22:10:54 -0300

    Create TesteCon.java

---
+++ aula-12-atividade/TesteCon.java
@@ -0,0 +1,12 @@
+
+package entidade;
+
+import java.sql.SQLException;
+
+
+public class TesteCon {
+    public static void main (String[] args) throws SQLException {
+        DAO cx = new DAO();
+        cx.abrirBanco();
+    }
+}
