2aa723e143e1f0ac051dad150d0ec45421cedf5e
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-11-11 09:38:47 -0300

    Update ManterAluno.java

--- aula-11e12/Pacotes/ManterAluno.java
+++ aula-11e12/Pacotes/ManterAluno.java
@@ -1,11 +1,19 @@
-// Classe criada dentro do pacote "entidade"
-
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package entidade;
 
 import controle.Aluno;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
 
+/**
+ *
+ * @author jes73
+ */
 public class ManterAluno extends DAO{
     public void inserir(Aluno a) throws Exception {
     try {
@@ -21,4 +29,40 @@ public class ManterAluno extends DAO{
         System.out.println("Erro " + e.getMessage());
     }
     }
+    
+    //metodo deletar aluno		
+ public void deletarAluno(Aluno a) throws Exception{
+	 abrirBanco();
+	 String query = "delete from aluno where codigo=?";
+	 pst=(PreparedStatement) con.prepareStatement(query);
+	 pst.setInt(1, a.getCodigo());
+	 pst.execute();
+        JOptionPane.showMessageDialog(null, "Aluno deletado com sucesso!");
+	fecharBanco();
+     }
+ 
+ // metodo listar 
+ public ArrayList<Aluno> PesquisarTudo () throws Exception {
+       ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+         try{
+         abrirBanco();  
+         String query = "select * FROM aluno";
+         pst = (PreparedStatement) con.prepareStatement(query);
+         ResultSet tr = pst.executeQuery();
+         Aluno a ;
+         while (tr.next()){               
+           a = new Aluno();
+           a.setCodigo(tr.getInt("codigo"));
+           a.setNome(tr.getString("nome"));
+           a.setEmail(tr.getString("email"));
+           alunos.add(a);
+         } 
+         fecharBanco();
+       }catch (Exception e){
+           System.out.println("Erro " + e.getMessage());
+     } 
+       return alunos;
+     }
+ 
+ 
 }
