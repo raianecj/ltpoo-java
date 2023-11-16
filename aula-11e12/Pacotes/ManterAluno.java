062e4d345c349d19ce277a5d54d63f61222f62fa
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-11-16 11:55:45 -0300

    Update ManterAluno.java

--- aula-11e12/Pacotes/ManterAluno.java
+++ aula-11e12/Pacotes/ManterAluno.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entidade;
 
 import controle.Aluno;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 
 /**
@@ -30,23 +26,12 @@ public class ManterAluno extends DAO{
     }
     }
     
-    //metodo deletar aluno		
- public void deletarAluno(Aluno a) throws Exception{
-	 abrirBanco();
-	 String query = "delete from aluno where codigo=?";
-	 pst=(PreparedStatement) con.prepareStatement(query);
-	 pst.setInt(1, a.getCodigo());
-	 pst.execute();
-        JOptionPane.showMessageDialog(null, "Aluno deletado com sucesso!");
-	fecharBanco();
-     }
- 
- // metodo listar 
- public ArrayList<Aluno> PesquisarTudo () throws Exception {
+    //listando Alunos
+            public ArrayList<Aluno> PesquisarTudo () throws Exception {
        ArrayList<Aluno> alunos = new ArrayList<Aluno>();
          try{
          abrirBanco();  
-         String query = "select * FROM aluno";
+         String query = "select codigo, nome FROM aluno";
          pst = (PreparedStatement) con.prepareStatement(query);
          ResultSet tr = pst.executeQuery();
          Aluno a ;
@@ -54,7 +39,6 @@ public class ManterAluno extends DAO{
            a = new Aluno();
            a.setCodigo(tr.getInt("codigo"));
            a.setNome(tr.getString("nome"));
-           a.setEmail(tr.getString("email"));
            alunos.add(a);
          } 
          fecharBanco();
@@ -63,6 +47,37 @@ public class ManterAluno extends DAO{
      } 
        return alunos;
      }
- 
- 
+  // pesquisar aluno          
+  public void PesquisarRegistro(Aluno a) throws Exception {
+        try {
+            abrirBanco();
+            String query = "select * FROM aluno where codigo=?";
+            pst = (PreparedStatement) con.prepareStatement(query);
+            pst.setInt(1, a.getCodigo());
+            ResultSet tr = pst.executeQuery();
+            if (tr.next()) {
+                a.setCodigo(tr.getInt("codigo"));
+                a.setNome(tr.getString("nome"));
+                a.setEmail(tr.getString("email"));
+            } else {
+              //  JOptionPane.showMessageDialog(null, "Nenhum resultado encontrado! ");
+            }
+            fecharBanco();
+        } catch (Exception e) {
+            System.out.println("Erro " + e.getMessage());
+        }
+    }    
+  // alterar aluno
+  public void editarAluno(Aluno a) throws Exception {
+        abrirBanco();
+        //JOptionPane.showMessageDialog(null, a.getNome()+ a.getEmail() + a.getIdade());
+        String query = "UPDATE aluno set nome = ?,email = ? where codigo=?";
+        pst = (PreparedStatement) con.prepareStatement(query);
+        pst.setString(1, a.getNome());
+        pst.setString(2, a.getEmail());
+        pst.setInt(3, a.getCodigo());
+        pst.executeUpdate();
+        fecharBanco();
+    }
+            
 }
