e8c28c44261510c88f2fcdbfb50a886ba893612f
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-11-28 10:33:06 -0300

    Create Livro.java

---
+++ EstanteLivro/control/Livro.java
@@ -0,0 +1,70 @@
+package control;
+/**
+ * @author Raiane
+ */
+public class Livro {
+    private int codigo;
+    private String titulo;
+    private String autor;
+    private String editora;
+    private String ano;
+    private String paginas;
+    private String idioma;
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(String paginas) {
+        this.paginas = paginas;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+}
+
