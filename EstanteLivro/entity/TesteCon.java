ca827a495f37cfa42590cb0f3f03b91f2fa39725
Raiane Carvalho <39846447+raianecj@users.noreply.github.com>
2023-11-28 10:39:01 -0300

    Create TesteCon.java

---
+++ EstanteLivro/entity/TesteCon.java
@@ -0,0 +1,10 @@
+package entity;
+
+import java.sql.SQLException;
+
+public class TesteCon {
+    public static void main (String[] args) throws SQLException {
+        DAO cx = new DAO();
+        cx.abrirBanco();
+    }
+}
